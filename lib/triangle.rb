68a9e6d84857008c5746840cf4d6ea84ff874132
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-29 21:15:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_accessor :first_side,:second_side,:third_side
+  
+   class TriangleError < StandardError
+     
+   end
+   
+  def initialize(first_side,second_side,third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+  end 
+  
+  def kind 
+    if @first_side == @second_side && @first_side == @third_side
+      :equilateral
+    elsif @first_side == @second_side || @first_side == @third_side || @second_side == @third_side
+      :isosceles
+    elsif @first_side != @second_side && @first_side != @third_side && @second_side != @third_side
+      :scalene
+    elsif @first_side == 0 && @second_side == 0 && @third_side ==0
+      raise TriangleError
+    end 
+  end 
+ 
 end
